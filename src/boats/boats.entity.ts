bfdb269971eec08554165fcd138f3a18ba7814b0
Jack Gitter <jack.gitter@gmail.com>
2025-07-01 13:48:09 -0400

    nice

--- src/boats/boats.entity.ts
+++ src/boats/boats.entity.ts
@@ -1,5 +1,6 @@
 import { Column, Entity } from 'typeorm';
 import { BOAT_CONDITION } from './boats.enums';
+import { ConflictException } from '@nestjs/common';
 
 @Entity('boat')
 export class Boat {
@@ -15,8 +16,11 @@ export class Boat {
   @Column({ name: 'name', type: 'varchar' })
   public name: string;
 
-  @Column({ name: 'name', type: 'enum', enum: BOAT_CONDITION })
+  @Column({ name: 'condition', type: 'enum', enum: BOAT_CONDITION })
   public condition: BOAT_CONDITION;
+
+  @Column({ name: 'currentlyRented', type: 'boolean' })
+  public currentlyRented: boolean;
 
   constructor(
     price: number,
@@ -24,15 +28,28 @@ export class Boat {
     capacity: number,
     name: string,
     condition: BOAT_CONDITION,
+    currentlyRented: boolean,
   ) {
     this.price = price;
     this.topSpeedInKnots = topSpeedInKnots;
     this.capacity = capacity;
     this.name = name;
     this.condition = condition;
+    this.currentlyRented = currentlyRented;
   }
 
   updateCondition(newCondition: BOAT_CONDITION) {
     this.condition = newCondition;
+  }
+
+  rentBoat() {
+    if (this.currentlyRented) {
+      throw new ConflictException('Boat is currently rented already!');
+    }
+    this.currentlyRented = true;
+  }
+
+  returnBoat() {
+    this.currentlyRented = false;
   }
 }

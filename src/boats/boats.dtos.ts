64ccef09d2e8760062ae0fbf8fbd5a811b26d626
Jack Gitter <jack.gitter@gmail.com>
2025-07-01 14:21:51 -0400

    nice

--- src/boats/boats.dtos.ts
+++ src/boats/boats.dtos.ts
@@ -1,6 +1,12 @@
+import { IsNumber, IsString } from 'class-validator';
+
 export class CreateBoatDTO {
+  @IsNumber()
   public price: number;
+  @IsNumber()
   public topSpeedInKnots: number;
+  @IsNumber()
   public capacity: number;
+  @IsString()
   public name: string;
 }

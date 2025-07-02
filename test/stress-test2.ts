30b35aa672ab1d49059215fc1de2122a8dc370df
Jack Gitter <jack.gitter@gmail.com>
2025-07-02 11:29:57 -0400

    nice

--- test/stress-test2.ts
+++ test/stress-test2.ts
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-  iterations: 10,
+  iterations: 100,
   vus: 10,
   thresholds: {
     http_req_duration: ['p(95)<50'],
@@ -25,7 +25,12 @@ export default function () {
     });
     sleep(1);
     if (res.status === 200) {
-      http.patch(`http://localhost:3000/boats/return/${availableBoat.id}`);
+      const res2 = http.patch(
+        `http://localhost:3000/boats/return/${availableBoat.id}`,
+      );
+      check(res2, {
+        'status was 200': (r) => r.status === 200,
+      });
     }
   }
   sleep(1);
